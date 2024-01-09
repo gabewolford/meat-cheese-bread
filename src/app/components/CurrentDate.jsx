091e3d37a3f0e0a9b68303fc3dac9bafc4f12eee
gabewolford <gabewolford@gmail.com>
2024-01-08 16:37:22 -0800

    added loading state

--- src/app/components/CurrentDate.jsx
+++ src/app/components/CurrentDate.jsx
@@ -4,27 +4,38 @@ import { useEffect, useState } from "react";
 
 export default function CurrentDate() {
   const [currentDate, setCurrentDate] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const updateDate = () => {
-      const options = {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-      };
-      const today = new Date().toLocaleDateString(undefined, options);
-      setCurrentDate(today);
+    const fetchData = async () => {
+      try {
+        const options = {
+          weekday: "long",
+          month: "long",
+          day: "numeric",
+          year: "numeric",
+        };
+        const today = new Date().toLocaleDateString(undefined, options);
+        setCurrentDate(today);
+      } catch (error) {
+        console.error("Error fetching date:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
-    // Initial update
-    updateDate();
+    fetchData();
 
     // Update date every minute
-    const intervalId = setInterval(updateDate, 60000);
+    const intervalId = setInterval(fetchData, 60000);
 
     // Cleanup interval on component unmount
     return () => clearInterval(intervalId);
   }, []);
+
+  if (isLoading) {
+    return "Loading date..."; // You can return a loading state or placeholder here
+  }
+
   return currentDate;
 }
