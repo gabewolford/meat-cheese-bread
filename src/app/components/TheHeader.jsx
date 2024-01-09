f61cd7e18ce91944952b0896538e0704edecea27
gabewolford <gabewolford@gmail.com>
2024-01-08 17:08:55 -0800

    adjusted header spacing and text size

--- src/app/components/TheHeader.jsx
+++ src/app/components/TheHeader.jsx
@@ -9,7 +9,7 @@ export default function TheHeader() {
     <header className="flex flex-col border-2 border-black mx-4 mt-4 md:mx-6 md:mt-6">
       <section className="flex flex-col gap-2 lg:gap-6 p-4 lg:p-6">
         <p
-          className={`${supplyMonoRegular.className} text-xs md:text-base text-center uppercase`}
+          className={`${supplyMonoRegular.className} text-xs lg:text-base text-center uppercase`}
         >
           <CurrentDate />
         </p>
@@ -18,22 +18,22 @@ export default function TheHeader() {
           <Image
             src={headerLogo}
             alt="Meat Cheese Bread"
-            className="w-full md:w-5/6"
+            className="w-full md:w-5/6 mb-1.5"
           />
         </Link>
 
         <div
           className={`${supplyMonoRegular.className} grid grid-cols-2 gap-2 md:gap-0 md:flex md:flex-row`}
         >
-          <p className="order-2 col-span-1 md:order-1 md:w-1/3 uppercase text-xs md:text-base">
+          <p className="order-2 col-span-1 md:order-1 md:w-1/3 uppercase text-xs lg:text-base">
             7am - 3pm Daily
           </p>
-          <p className="order-1 mb-2 md:mb-0 col-span-2 md:order-2 md:w-1/3 flex justify-center uppercase text-xs md:text-base">
+          <p className="order-1 mb-2 md:mb-0 col-span-2 md:order-2 md:w-1/3 flex justify-center uppercase text-xs lg:text-base">
             A Portland, Oregon Restaurant
           </p>
           <a
             href="tel:+15032341700"
-            className="order-3 col-span-1 md:order-3 md:w-1/3 flex justify-end text-xs md:text-base"
+            className="order-3 col-span-1 md:order-3 md:w-1/3 flex justify-end text-xs lg:text-base"
           >
             503-234-1700
           </a>
