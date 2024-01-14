eebd94e03c7b875664d0f8af42cc0c6e6f746621
gabewolford <gabewolford@gmail.com>
2024-01-13 20:11:16 -0800

    revalidating at most once per minute

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -2,8 +2,8 @@ import { client } from "../../../sanity/lib/client";
 
 export async function GetMenuData() {
   const menuData = await client.fetch(`*[_type == "menu"]`, {
-    cache: "no-store",
-    // revalidate: 3600,
+    // cache: "no-store",
+    revalidate: 60,
   });
   console.log(menuData);
 
