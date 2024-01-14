b1bdf6316c39a0d7bae70d1ea51dc33259ffc5db
gabewolford <gabewolford@gmail.com>
2024-01-13 20:07:40 -0800

    added revalidate comment back in, incase of setting time

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -3,6 +3,7 @@ import { client } from "../../../sanity/lib/client";
 export async function GetMenuData() {
   const menuData = await client.fetch(`*[_type == "menu"]`, {
     cache: "no-store",
+    // revalidate: 3600,
   });
   console.log(menuData);
 
