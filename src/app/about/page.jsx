e0bf4fb6244d25290f5785ba5317332138ee3699
gabewolford <gabewolford@gmail.com>
2024-01-14 17:12:49 -0800

    started about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,9 @@
 import PhotoRightCTA from "../components/PhotoRightCTA";
+import { supplyMonoUltralight } from "../styles/fonts";
+import JohnLogo from "../components/Logos/JohnLogo";
+import DoubleImage from "../components/DoubleImage";
+import red from "../../../public/images/red.png";
+import yellow from "../../../public/images/yellow.png";
 
 export const metadata = {
   metadataBase: new URL("https://meatcheesebread.com"),
@@ -36,7 +41,41 @@ export const metadata = {
 export default function AboutPage() {
   return (
     <>
-      <section className="w-full h-[500px] p-4 md:p-6">About Page</section>
+      <section className="flex flex-col w-full h-fit">
+        <div className="flex flex-col text-center p-6 md:p-16 gap-8">
+          <h2 className="text-3xl">About Us</h2>
+          <div
+            className={`${supplyMonoUltralight.className} flex flex-col lg:flex-row items-center text-left gap-6 w-full`}
+          >
+            <div className="flex flex-col gap-8 w-full lg:w-1/2 lg:p-12 lg:text-lg">
+              <p>
+                Located in the Buckman neighborhood of southeast Portland, Meat
+                Cheese Bread is a sandwich shop that uses fresh, local products
+                of superior quality in a clean and vibrant space. In our open
+                kitchen we focus on fresh, high-quality ingredients and intense
+                flavors.
+              </p>
+              <p>
+                Using the products of local farmers and ranchers, combined with
+                hand-made bread in our creative recipes, our goal is to raise
+                the standard of the sandwich in the Northwest while contributing
+                to our local community.
+              </p>
+            </div>
+            <div className="flex flex-col w-full max-h-[500px] lg:w-1/2 justify-center items-center">
+              <JohnLogo fillColorHash="#000000" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <DoubleImage
+        photo1={red}
+        altText1="placeholder"
+        photo2={yellow}
+        altText2="placeholder"
+      />
+
       <PhotoRightCTA
         heading="Are you hungry yet?"
         linkTo="/menu"
